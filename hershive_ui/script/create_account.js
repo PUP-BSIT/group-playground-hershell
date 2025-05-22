9ebf1a2cf862c6d8c0bc0bb6e038ad08fcc106d1
jallaine <jallainesiervo143@gmail.com>
2025-05-23 01:28:18 +0800

    add implementation with Image Delete Option

--- hershive_ui/script/create_account.js
+++ hershive_ui/script/create_account.js
@@ -1,15 +1,21 @@
-const profileInput = document.getElementById('profileImage');
-  const preview = document.getElementById('imagePreview');
+const profileInput = document.getElementById("profile_image");
+const preview = document.getElementById("image_preview");
+const removeBtn = document.getElementById("remove_image");
 
-  profileInput.addEventListener('change', function () {
-    const file = this.files[0];
-    if (file) {
-      const reader = new FileReader();
+profileInput.addEventListener("change", function () {
+  const file = this.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      preview.innerHTML = `<img src="${e.target.result}" alt="Profile Image">`;
+      removeBtn.style.display = "flex";
+    };
+    reader.readAsDataURL(file);
+  }
+});
 
-      reader.onload = function (e) {
-        preview.innerHTML = `<img src="${e.target.result}"alt="Profile Image">`;
-      };
-
-      reader.readAsDataURL(file);
-    }
-  });
+removeBtn.addEventListener("click", function () {
+  profileInput.value = "";
+  preview.innerHTML = "<span>add image</span>";
+  removeBtn.style.display = "none";
+});
